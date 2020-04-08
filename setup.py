d3980866f8227cb6b29e9b24b2c7ade00784fa21
Dave Zeber <dzeber@mozilla.com>
2020-04-08 14:49:16 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ extras = {
 
 setup(
     name="dscitools",
-    version="0.1.0",
+    version="0.1.1",
     description="Tools for data science with Jupyter/Pandas/Spark",
     long_description=long_description,
     long_description_content_type='text/markdown',
