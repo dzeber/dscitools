3ed040e5fae549d87f96d0b17c88f6738682c5c7
Dave Zeber <dzeber@mozilla.com>
2020-06-02 15:47:27 -0500

    Format setup.py with black

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup, find_packages
+
 ## Python 2 compat
 from io import open
 
 # Get the long description from the README file
-with open("README.md", encoding='utf-8') as f:
+with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
 
 test_deps = [
@@ -15,19 +16,17 @@ test_deps = [
     "ipykernel",
     "jupyter_client",
     "moto",
-    "dill"
+    "dill",
 ]
 
-extras = {
-    "testing": test_deps
-}
+extras = {"testing": test_deps}
 
 setup(
     name="dscitools",
     version="0.1.2",
     description="Tools for data science with Jupyter/Pandas/Spark",
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     url="https://github.com/dzeber/dscitools",
     author="David Zeber",
     author_email="dzeber@mozilla.com",
@@ -45,15 +44,10 @@ setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
-        "Topic :: Scientific/Engineering :: Information Analysis"
+        "Topic :: Scientific/Engineering :: Information Analysis",
     ],
     packages=find_packages(exclude=["tests"]),
-    install_requires=[
-        "numpy",
-        "pandas",
-        "six",
-        "boto3"
-    ],
+    install_requires=["numpy", "pandas", "six", "boto3",],
     tests_require=test_deps,
-    extras_require=extras
+    extras_require=extras,
 )
